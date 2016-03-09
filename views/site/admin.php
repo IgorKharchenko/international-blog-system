03cb3e6122cdacc1d2fdbc6085a5d9556b1b51fd
IgorKharchenko <hatand@bk.ru>
2016-03-09 06:09:30 +0600

    Added Bootstrap Tabs

--- views/site/admin.php
+++ views/site/admin.php
@@ -2,51 +2,26 @@
 use yii\helpers\Html;
 use yii\bootstrap\Button;
 use yii\bootstrap\ButtonDropdown;
+use yii\bootstrap\Tabs;
+
 /* @var $this yii\web\View */
 
 $this->title = 'Admin Page';
 ?>
 
 <div class="site-admin">
-
-    <h3>Blog Console</h3>
-    <hr/>
-    <?php if($assign_permitted): ?>
-        <?= Html::a('Assign privilegies to users', ['users/assign'], ['class' => 'btn btn-success', 'style' => 'margin:5px']) ?>
-    <?php endif; ?>
-        <?= Html::a('List of All Users Info', ['users/index'], ['class' => 'btn btn-info', 'style' => 'margin:5px']) ?>
-    <hr/>
-    <h4>In this page you can: </h4>
-    <table>
-        <tr>
-            <td>
-                <?= Html::a('Create Post', ['post/create'], ['class' => 'btn btn-warning']) ?>
-            </td>
-            <td>
-                <?= ButtonDropdown::widget([
-                    'label' => 'View Our Posts',
-                    'options' => [
-                        'class' => 'btn btn-warning',
-                        'style' => 'margin:5px',
-                    ],
-                    'dropdown' => [
-                        'items' => [
-                            [
-                                'label' => 'With the Publish status',
-                                'url' => ['post/ourposts', 'status' => 'publish'],
-                            ],
-                            [
-                                'label' => 'With the Draft status',
-                                'url' => ['post/ourposts', 'status' => 'draft'],
-                            ],
-                            [
-                                'label' => 'All posts',
-                                'url' => ['post/ourposts', 'status' => 'all'],
-                            ],
-                        ]
-                    ]
-                ]); ?>
-            </td>
-        </tr>
-    </table>
+    <?php
+        echo Tabs::widget([
+            'items' => [
+                [
+                    'label' => 'Users',
+                    'content' => $this->render('admin_UsersTab', ['assign_permitted' => $assign_permitted]),
+                ],
+                [
+                    'label' => 'Posts',
+                    'content' => $this->render('admin_PostsTab'),
+                ],
+            ],
+        ]);
+    ?>
 </div>
